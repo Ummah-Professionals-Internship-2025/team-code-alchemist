90812ed75af556665270103dcfd952e776b107ef
Lena A <93883549+Lenaa002@users.noreply.github.com>
2025-08-10 14:10:44 -0400

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminApproval from "./admin/AdminApproval";
-import MentorCreateUser from "./mentors/MentorCreateUser";
 import MentorApplicationForm from "./mentors/MentorApplicationForm";
+import MentorCreateUser from "./mentors/MentorCreateUser";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/mentor-application" replace />} />
+        <Route path="/admin" element={<AdminApproval />} />
         <Route path="/mentor-application" element={<MentorApplicationForm />} />
         <Route path="/create-password" element={<MentorCreateUser />} />
-        <Route path="/admin" element={<AdminApproval />} />
-        <Route path="/mentor-create" element={<MentorCreateUser />} />
-        <Route path="/mentor-create-user" element={<MentorCreateUser />} />
-        <Route path="*" element={<h1 style={{padding:24}}>Page not found</h1>} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
