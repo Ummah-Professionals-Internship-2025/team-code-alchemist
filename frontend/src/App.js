f80f7365905cc6fe72e806d55a92632e9137d9dd
Lena A <93883549+Lenaa002@users.noreply.github.com>
2025-08-10 14:23:20 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminApproval from "./admin/AdminApproval";
 import MentorApplicationForm from "./mentors/MentorApplicationForm";
 import MentorCreateUser from "./mentors/MentorCreateUser";
+import AdminApproval from "./admin/AdminApproval";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/admin" element={<AdminApproval />} />
-        <Route path="/mentor-application" element={<MentorApplicationForm />} />
+        <Route path="/" element={<MentorApplicationForm />} />
         <Route path="/create-password" element={<MentorCreateUser />} />
-        <Route path="*" element={<h1>Page Not Found</h1>} />
+        <Route path="/admin" element={<AdminApproval />} />
+        <Route path="*" element={<h2>page not found</h2>} />
       </Routes>
     </Router>
   );
