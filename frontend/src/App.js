d305fe4056531594878ccec69b5c25cadcdf80e2
Noor EL-Hawwat <ne152@scarletmail.rutgers.edu>
2025-08-26 13:58:26 -0400

    designs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,7 @@ import react from 'react';
 import workers from './workers.png';
 
 function App() {
-    return(
-    <img 
-        src={design} 
-        alt="Design"
-    style={{
-        width: '100%',
-        height: 'auto',
-        objectFit: 'cover',
-        display: 'block',
-        margin: 0,
-        padding: 0,
-        }}
-    />
-);
+    
 }
 
 export default App;
